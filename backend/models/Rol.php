412d6607fa8ca4d31882f64f498a84bfcb1d8bed
Cecilio Cauich <celiocatu@gmail.com>
2021-11-10 15:54:52 -0600

    crud de usuarios,perfiles y roles

---
+++ backend/models/Rol.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "rol".
+ *
+ * @property int $id
+ * @property int $nombre
+ *
+ * @property User $id0
+ */
+class Rol extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'rol';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'nombre'], 'required'],
+            [['id', 'nombre'], 'integer'],
+            [['id'], 'unique'],
+            [['id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['id' => 'id_rol']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+        ];
+    }
+
+    /**
+     * Gets query for [[Id0]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getId0()
+    {
+        return $this->hasOne(User::className(), ['id_rol' => 'id']);
+    }
+}
---
+++ backend/models/User.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $auth_key
+ * @property string $password_hash
+ * @property string|null $password_reset_token
+ * @property string $email
+ * @property int $status
+ * @property int $created_at
+ * @property int $updated_at
+ * @property string|null $verification_token
+ * @property int $id_rol
+ * @property int $id_perfil
+ *
+ * @property Perfil $perfil
+ * @property Rol $rol
+ */
+class User extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'auth_key', 'password_hash', 'email', 'created_at', 'updated_at', 'id_rol', 'id_perfil'], 'required'],
+            [['status', 'created_at', 'updated_at', 'id_rol', 'id_perfil'], 'integer'],
+            [['username', 'password_hash', 'password_reset_token', 'email', 'verification_token'], 'string', 'max' => 255],
+            [['auth_key'], 'string', 'max' => 32],
+            [['username'], 'unique'],
+            [['email'], 'unique'],
+            [['password_reset_token'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'auth_key' => 'Auth Key',
+            'password_hash' => 'Password Hash',
+            'password_reset_token' => 'Password Reset Token',
+            'email' => 'Email',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'verification_token' => 'Verification Token',
+            'id_rol' => 'Id Rol',
+            'id_perfil' => 'Id Perfil',
+        ];
+    }
+
+    /**
+     * Gets query for [[Perfil]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPerfil()
+    {
+        return $this->hasOne(Perfil::className(), ['id' => 'id_perfil']);
+    }
+
+    /**
+     * Gets query for [[Rol]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getRol()
+    {
+        return $this->hasOne(Rol::className(), ['id' => 'id_rol']);
+    }
+}
