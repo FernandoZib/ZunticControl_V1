bf60db0c56cc04d72fb7689e96da74aa1af490fd
Darwin Cocom <darwin.cocommis@itsva.edu.mx>
2021-11-10 08:51:38 -0600

    Bd

--- bd_v1.sql
+++ bd_v1.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-10-2021 a las 18:02:12
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.4
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 10-11-2021 a las 14:47:09
+-- Versión del servidor: 5.7.31
+-- Versión de PHP: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,14 +27,16 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `cuenta`
 --
 
-CREATE TABLE `cuenta` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `cuenta`;
+CREATE TABLE IF NOT EXISTS `cuenta` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `concepto` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `monto` decimal(65,0) NOT NULL,
   `creado_el` datetime NOT NULL,
   `actualizado_el` datetime NOT NULL,
   `creado_por` int(11) NOT NULL,
-  `actualizado_por` int(11) NOT NULL
+  `actualizado_por` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -43,14 +45,18 @@ CREATE TABLE `cuenta` (
 -- Estructura de tabla para la tabla `estado`
 --
 
-CREATE TABLE `estado` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `estado`;
+CREATE TABLE IF NOT EXISTS `estado` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_limpieza` int(11) NOT NULL,
   `id_ocupacion` int(11) NOT NULL,
   `creado_el` datetime NOT NULL,
   `actualizado_el` datetime NOT NULL,
   `creado_por` int(11) NOT NULL,
-  `actualizado_por` int(11) NOT NULL
+  `actualizado_por` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_limpieza` (`id_limpieza`),
+  KEY `id_ocupacion` (`id_ocupacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -59,10 +65,12 @@ CREATE TABLE `estado` (
 -- Estructura de tabla para la tabla `estancia`
 --
 
-CREATE TABLE `estancia` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `estancia`;
+CREATE TABLE IF NOT EXISTS `estancia` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `fecha_entrada` datetime NOT NULL,
-  `fecha_salida` datetime NOT NULL
+  `fecha_salida` datetime NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -71,11 +79,15 @@ CREATE TABLE `estancia` (
 -- Estructura de tabla para la tabla `habitacion`
 --
 
-CREATE TABLE `habitacion` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `habitacion`;
+CREATE TABLE IF NOT EXISTS `habitacion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_tipo_habitacion` int(11) NOT NULL,
   `id_planta` int(11) NOT NULL,
-  `id_estado` int(11) NOT NULL
+  `id_estado` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_tipo_habitacion` (`id_tipo_habitacion`),
+  KEY `id_estado` (`id_estado`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -84,8 +96,9 @@ CREATE TABLE `habitacion` (
 -- Estructura de tabla para la tabla `huesped`
 --
 
-CREATE TABLE `huesped` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `huesped`;
+CREATE TABLE IF NOT EXISTS `huesped` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `telefono` int(15) NOT NULL,
   `correo` int(255) NOT NULL,
@@ -95,7 +108,11 @@ CREATE TABLE `huesped` (
   `creado_el` int(11) NOT NULL,
   `actualizado_el` int(11) NOT NULL,
   `creado_por` datetime NOT NULL,
-  `actualizado_por` datetime NOT NULL
+  `actualizado_por` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_cuenta` (`id_cuenta`),
+  KEY `id_estancia` (`id_estancia`),
+  KEY `id_habitacion` (`id_habitacion`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -104,9 +121,11 @@ CREATE TABLE `huesped` (
 -- Estructura de tabla para la tabla `limpieza`
 --
 
-CREATE TABLE `limpieza` (
-  `id` int(11) NOT NULL,
-  `titulo` int(11) NOT NULL
+DROP TABLE IF EXISTS `limpieza`;
+CREATE TABLE IF NOT EXISTS `limpieza` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -115,9 +134,11 @@ CREATE TABLE `limpieza` (
 -- Estructura de tabla para la tabla `migration`
 --
 
-CREATE TABLE `migration` (
+DROP TABLE IF EXISTS `migration`;
+CREATE TABLE IF NOT EXISTS `migration` (
   `version` varchar(180) COLLATE utf8_unicode_ci NOT NULL,
-  `apply_time` int(11) DEFAULT NULL
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -135,9 +156,40 @@ INSERT INTO `migration` (`version`, `apply_time`) VALUES
 -- Estructura de tabla para la tabla `ocupacion`
 --
 
-CREATE TABLE `ocupacion` (
+DROP TABLE IF EXISTS `ocupacion`;
+CREATE TABLE IF NOT EXISTS `ocupacion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titulo` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil`
+--
+
+DROP TABLE IF EXISTS `perfil`;
+CREATE TABLE IF NOT EXISTS `perfil` (
   `id` int(11) NOT NULL,
-  `titulo` int(11) NOT NULL
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `apellidos` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `telefono` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `correo` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `rol`
+--
+
+DROP TABLE IF EXISTS `rol`;
+CREATE TABLE IF NOT EXISTS `rol` (
+  `id` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -146,12 +198,14 @@ CREATE TABLE `ocupacion` (
 -- Estructura de tabla para la tabla `tipo_habitacion`
 --
 
-CREATE TABLE `tipo_habitacion` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `tipo_habitacion`;
+CREATE TABLE IF NOT EXISTS `tipo_habitacion` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `tipo` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `precio` decimal(65,0) NOT NULL,
   `numero` int(11) NOT NULL,
-  `planta` int(11) NOT NULL
+  `planta` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -160,157 +214,37 @@ CREATE TABLE `tipo_habitacion` (
 -- Estructura de tabla para la tabla `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `status` smallint(6) NOT NULL DEFAULT 10,
+  `status` smallint(6) NOT NULL DEFAULT '10',
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
-  `verification_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `verification_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `id_rol` int(11) NOT NULL,
+  `id_perfil` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`),
+  KEY `id_rol` (`id_rol`),
+  KEY `id_perfil` (`id_perfil`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`) VALUES
-(1, 'cecilio', 'aOPjxqVxGnXsQB6NKYcfvBWHzMt5zKHJ', '$2y$13$PLqdh4ycy7lhL1UPjQXqQehhvzBpl79bxvuSzDv.9IabhYccnG.Ky', NULL, 'cecilio@gmail.com', 10, 1634072493, 1634072493, 'Ras2PQITCcq9i8bAVbBDTS3h8CNBNPgk_1634072493');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `cuenta`
---
-ALTER TABLE `cuenta`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `estado`
---
-ALTER TABLE `estado`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_limpieza` (`id_limpieza`),
-  ADD KEY `id_ocupacion` (`id_ocupacion`);
-
---
--- Indices de la tabla `estancia`
---
-ALTER TABLE `estancia`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `habitacion`
---
-ALTER TABLE `habitacion`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_tipo_habitacion` (`id_tipo_habitacion`),
-  ADD KEY `id_estado` (`id_estado`);
-
---
--- Indices de la tabla `huesped`
---
-ALTER TABLE `huesped`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_cuenta` (`id_cuenta`),
-  ADD KEY `id_estancia` (`id_estancia`),
-  ADD KEY `id_habitacion` (`id_habitacion`);
-
---
--- Indices de la tabla `limpieza`
---
-ALTER TABLE `limpieza`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `migration`
---
-ALTER TABLE `migration`
-  ADD PRIMARY KEY (`version`);
-
---
--- Indices de la tabla `ocupacion`
---
-ALTER TABLE `ocupacion`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tipo_habitacion`
---
-ALTER TABLE `tipo_habitacion`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `cuenta`
---
-ALTER TABLE `cuenta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `estado`
---
-ALTER TABLE `estado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `estancia`
---
-ALTER TABLE `estancia`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `habitacion`
---
-ALTER TABLE `habitacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `huesped`
---
-ALTER TABLE `huesped`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `limpieza`
---
-ALTER TABLE `limpieza`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `ocupacion`
---
-ALTER TABLE `ocupacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `tipo_habitacion`
---
-ALTER TABLE `tipo_habitacion`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+INSERT INTO `user` (`id`, `username`, `auth_key`, `password_hash`, `password_reset_token`, `email`, `status`, `created_at`, `updated_at`, `verification_token`, `id_rol`, `id_perfil`) VALUES
+(1, 'cecilio', 'aOPjxqVxGnXsQB6NKYcfvBWHzMt5zKHJ', '$2y$13$PLqdh4ycy7lhL1UPjQXqQehhvzBpl79bxvuSzDv.9IabhYccnG.Ky', NULL, 'cecilio@gmail.com', 10, 1634072493, 1634072493, 'Ras2PQITCcq9i8bAVbBDTS3h8CNBNPgk_1634072493', 0, 0),
+(2, 'angel.nahuat', 'mnpe6tOkJiYJJDdG9wgSbrgQZz0Xj2fz', '$2y$13$naWqDKGSaB0d7gXaIb6b6Oy1aTzViFxit6Q5rMNsf8gYn2EH4Fyzu', NULL, 'angel.nn@gmail.com', 10, 1636523008, 1636523008, '2rtdxUlX88FvTVoja4f4yKA14YYLlhE3_1636523008', 0, 0),
+(3, 'eduin.nahuat', 'M5dSTk9iHOWA5A5cHDC37VT0HD5pAJ_3', '$2y$13$mZ4OQbLFRatZ.qM4NIuCYuq6/IGq3F/Rfik3eaD7rDr3ZfA7enhLO', NULL, 'eduin.nahuatnoh@gmail.com', 10, 1636524169, 1636524169, 'GorTcje76uwWvVlOOvsM76G7RyxExyWS_1636524169', 0, 0),
+(4, 'erika.aguilar', 'C09OXoQEH6eUSzd9Cq9JytE2qzpu10_9', '$2y$13$PwAa2aEaXxYSr9HYhCaprOStboAK5omCe3WfSXiLFhLoA30ZZOI.y', NULL, 'erika.aguilar98@gmail.com', 9, 1636554909, 1636554909, 'U2JI0NVqM6b14AH_lu3mZqclsn-6nDgh_1636554909', 0, 0);
 
 --
 -- Restricciones para tablas volcadas
@@ -338,6 +272,18 @@ ALTER TABLE `huesped`
   ADD CONSTRAINT `huesped_ibfk_2` FOREIGN KEY (`id_cuenta`) REFERENCES `cuenta` (`id`),
   ADD CONSTRAINT `huesped_ibfk_3` FOREIGN KEY (`id_estancia`) REFERENCES `estancia` (`id`),
   ADD CONSTRAINT `huesped_ibfk_4` FOREIGN KEY (`id_habitacion`) REFERENCES `habitacion` (`id`);
+
+--
+-- Filtros para la tabla `perfil`
+--
+ALTER TABLE `perfil`
+  ADD CONSTRAINT `perfil_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id_perfil`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Filtros para la tabla `rol`
+--
+ALTER TABLE `rol`
+  ADD CONSTRAINT `rol_ibfk_1` FOREIGN KEY (`id`) REFERENCES `user` (`id_rol`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
